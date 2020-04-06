df04025f383bac563f890b60103d03186d3f43c7
Chris Tippett <c.tippett@gmail.com>
2020-04-07 04:16:39 +1000

    refactor: post-setup script

--- scripts/post-setup.bash
+++ scripts/post-setup.bash
@@ -5,35 +5,109 @@ set -euo pipefail
 # shellcheck source=./utils.bash
 source "$(dirname "$0")/utils.bash"
 
-# get OS name
-osType="$(uname -s)"
+### OS utilities
+if [ "$LINUX" ]; then
+    log_info "ℹ️  Installing linux utilities"
+
+    is_installed htop || sudo apt install -y htop # install jq: command-line json processor
+    is_installed htop || sudo apt install -y htop # install htop: interactive process viewer
+    is_installed ncdu || sudo apt install -y ncdu # install ncdu: disk usage analyzer
+    is_installed ncdu || sudo apt install -y ncdu # install ncdu: disk usage analyzer
+
+    # install bat: a cat clone with wings (https://github.com/sharkdp/bat)
+    # note: package available in apt repository from 19.10
+    if ! is_installed bat; then
+        wget -q -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_amd64.deb
+        sudo dpkg -i /tmp/bat.deb && rm -f /tmp/bat.deb
+    fi
+
+    # install prettyping: wrapper around ping (http://denilson.sa.nom.br/prettyping/)
+    if ! is_installed prettyping; then
+        wget -q https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping
+        chmod +x prettyping
+        sudo mv -f prettyping /usr/local/bin
+    fi
+
+    # install 1password (https://support.1password.com/command-line-getting-started)
+    if ! is_installed op; then
+        wget -q -O /tmp/1password.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.9.4/op_linux_amd64_v0.9.4.zip
+        unzip -q -o /tmp/1password.zip -d /tmp/1password/
+        sudo mv -f /tmp/1password/op /usr/local/bin
+        rm -rfv /tmp/1password*
+    fi
+
+    # install doctl: digital ocean cli
+    if ! is_installed doctl; then
+        wget -q -O /tmp/doctl.tar.gz https://github.com/digitalocean/doctl/releases/download/v1.39.0/doctl-1.39.0-linux-amd64.tar.gz
+        tar xf /tmp/doctl.tar.gz -C /tmp/doctl
+        sudo mv -f /tmp/doctl/doctl /usr/local/bin
+        rm -rfv /tmp/doctl*
+    fi
+fi
+
+if [ "$MACOS" ]; then
+    # note: in most cases macos packages/applications should be specified and installed from Brewfile
+    log_info "ℹ️  Installing MacOS utilities"
+    is_installed op || brew cask install 1password # install op: 1password
+fi
+
+if [ "$WSL" ]; then
+    log_info "ℹ️  Installing Windows utilities"
+    username="$(cmd.exe /c "echo %USERNAME%" 2>/dev/null)" # get windows user name
+fi
 
 
-case "${osType}" in
-Linux*)
-    log_info "ℹ️  Installing Ubuntu extras"
-    # install improved cli tools
-    sudo apt-get install htop ncdu
-    # install bat (package only available in apt repository since 19.10)
-    wget -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_amd64.deb
-    sudo dpkg -i /tmp/bat.deb && rm -f /tmp/bat.deb
-    # install prettyping - http://denilson.sa.nom.br/prettyping/
-    curl -O https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping
-    chmod +x prettyping
-    sudo mv prettyping /usr/local/bin
-    ;;
-Darwin*)
-    log_info "ℹ️  Installing MacOS extras"
-    ;;
-*)
-    log_failure_and_exit "Script only supports macOS and Ubuntu"
-    ;;
-esac
+### Python utilities
+if isinstalled pipx; then
+    log_info "ℹ️  Installing Python utilities"
 
-# Python
-pipx install black
-pipx install awscli
-pipx install youtube-dl
-pipx install poetry
-pip install virtualenvwrapper && pip inject virtualenvwrapper virtualenv && mkdir $HOME/.virtualenvs
-mkdir -p $ZSH/custom/plugins/poetry && poetry completions zsh > $ZSH/custom/plugins/poetry/_poetry
+    is_installed black || pipx install black # install black: opinioned python formatter
+    is_installed aws || pipx install awscli # install awscli: amazon web services cli
+    is_installed youtube-dl || pipx install youtube-dl # install youtube-dl: cli video download tool
+
+    # install poetry: python package manager
+    if ! is_installed poetry; then
+        pipx install poetry
+        mkdir -p $ZSH/custom/plugins/poetry && poetry completions zsh > $ZSH/custom/plugins/poetry/_poetry
+
+    fi
+
+    # install virtualenvwrapper: wrapper around python's virtualenv
+    if ! is_installed virtualenvwrapper; then
+        pipx install virtualenvwrapper
+        pipx inject virtualenvwrapper virtualenv # required to resolve module import dependency
+        mkdir $HOME/.virtualenvs # should be set to the same value as $WORKON_HOME in ~/.zshrc
+    fi
+else
+    log_failure "Missing pipx installation, skipping install of Python utilities"
+fi
+
+
+### 1Password secrets
+log_info "ℹ️  Getting secrets and configuration files from 1Password"
+
+function get_document_label() {
+    local label="$1"
+    local doc="$(</dev/stdin)"
+    jq -cr --arg v "$label" '.details.sections[].fields[]? | select(.t == $v) | .v' <<<  "$doc"
+}
+
+function get_document() {
+    local item="$(op get item "$uuid")"
+    local path="$(get_document_label "path" <<< "$item" | sed -e 's#~#'"$HOME"'#')"
+    local chmod="$(get_document_label "chmod" <<< "$item")"
+    backup_file "$path"
+    op get document "$uuid" > "$path"
+    chmod ${chmod:-644} "$path"
+    log_success "Saved file to $path (chmod: ${chmod:-644})"
+}
+
+if [ -z "$OP_SESSION_my" ]; then
+    eval $(op signin "$op_subdomain")
+else
+    op list documents | jq -c -r '.[] | [.uuid, .overview.title] | @tsv' |
+    while IFS=$'\t' read -r uuid title; do
+        log_info "ℹ️  Getting document from 1Password: $title"
+        get_document $uuid </dev/null
+    done
+fi
